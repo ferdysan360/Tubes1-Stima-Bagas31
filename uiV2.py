757799d131aa979861cdf9b4f883b6a95c899ef6
Ferdy Santoso <36350020+ferdysan360@users.noreply.github.com>
2019-02-08 11:54:27 +0700

    Update uiV2.py
    Sudah bisa draw cards

--- uiV2.py
+++ uiV2.py
@@ -1,11 +1,21 @@
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition, FallOutTransition
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition,  SlideTransition
+from array import array
+import random
+import copy
 
-a = 1
-b = 3
-c = 7
-d = 4
+
+datainit = array('i')
+data = array('i')
+for i in range(1,14):
+    datainit.append(i)
+    datainit.append(i)
+    datainit.append(i)
+    datainit.append(i)
+
+data = copy.copy(datainit)
 
 # kivy file implementation
 Builder.load_string("""
@@ -14,81 +24,90 @@ Builder.load_string("""
         rows: 2
         BoxLayout:
             Label:
-                text: 'Nothing to see here'
+                font_size: 72
+                text: '24 Solver'
 
         BoxLayout:
             Button:
-                text: 'File input'
-                on_press:
+                font_size: 50
+                text: 'Start'
+                on_release:
                     root.manager.transition.direction = 'left'
                     root.manager.current = 'fileinput'
 
             Button:
-                text: 'Button input'
-                on_press:
-                    root.manager.transition.direction = 'left'
-                    root.manager.current = 'buttoninput'
+                font_size: 50
+                text: 'Exit'
+                on_release:
+                    root.CloseApp()
 
 <FileInputScreen>:
     GridLayout:
         rows: 2
-        BoxLayout:
+        GridLayout:
             rows: 2
-            Label:
-                id: label1
-                text: 'Label 1'
-            Label:
-                id: label2
-                text: 'Label 2'
-            Label:
-                id: label3
-                text: 'Label 3'
-            Label:
-                id: label4
-                text: 'Label 4'
+            BoxLayout:
+                Label:
+                    font_size: 50
+                    id: cardsleftlabel
+                    text: root.GetCardsLeft()
+
+            BoxLayout:
+                rows: 2
+                Label:
+                    font_size: 50
+                    id: label1
+                    text: 'No card'
+                Label:
+                    font_size: 50
+                    id: label2
+                    text: 'No card'
+                Label:
+                    font_size: 50
+                    id: label3
+                    text: 'No card'
+                Label:
+                    font_size: 50
+                    id: label4
+                    text: 'No card'
+
         GridLayout:
             rows: 2
             Button:
+                font_size: 50
+                id: drawbutton
                 text: 'Draw cards'
-                on_press:
-                    label1.text = root.GetNumber1()
-                    label2.text = root.GetNumber2()
-                    label3.text = root.GetNumber3()
-                    label4.text = root.GetNumber4()
+                disabled: False
+                on_release:
+                    label1.text = root.GetNumber()
+                    label2.text = root.GetNumber()
+                    label3.text = root.GetNumber()
+                    label4.text = root.GetNumber()
+                    cardsleftlabel.text = root.GetCardsLeft()
                     solvebutton.disabled = False
+                    if (root.GetNeff() == '0'): drawbutton.disabled = True
 
             Button:
+                font_size: 50
                 id: solvebutton
                 text: 'Solve'
                 disabled: True
-                on_press:
-                    root.manager.transition.direction = 'right'
+                on_release:
+                    root.manager.transition.direction = 'left'
                     root.manager.current = 'answer'
+
             Button:
+                font_size: 50
                 text: 'Reset'
-                on_press:
+                on_release:
                     root.CardsReset()
                     solvebutton.disabled = True
+                    drawbutton.disabled = False
 
             Button:
+                font_size: 50
                 text: 'Back to menu'
-                on_press:
-                    root.CardsReset()
-                    root.manager.transition.direction = 'right'
-                    root.manager.current = 'menu'
-
-<ButtonInputScreen>:
-    GridLayout:
-        rows: 2
-        BoxLayout:
-            Label:
-                font_size: 72
-                text: 'Hello, World!'
-
-        BoxLayout:
-            Button:
-                text: 'Back to menu'
-                on_press:
+                on_release:
                     root.manager.transition.direction = 'right'
                     root.manager.current = 'menu'
 
@@ -96,73 +115,75 @@ Builder.load_string("""
     BoxLayout:
         orientation: 'vertical'
         Label:
+            font_size: 72
             text: 'Answer : ' + root.Solve()
         
         Label:
+            font_size: 72
             text: 'Points : ' + root.Points()
     
         Button:
-            text: 'Back to menu'
-            on_press:
+            font_size: 50
+            text: 'Back'
+            on_release:
                 root.manager.transition.direction = 'right'
-                root.manager.current = 'menu'
+                root.manager.current = 'fileinput'
 """)
 
 # Declare screens
-
 class MenuScreen(Screen):
+    def CloseApp(self):
+        App.get_running_app().stop()
+        Window.close()
     pass
 
 class FileInputScreen(Screen):
-    global a
-    global b
-    global c
-    global d
 
-    def GetNumber1(self):
-        return str(a)
-
-    def GetNumber2(self):
-        return str(b)
-
-    def GetNumber3(self):
-        return str(c)
-
-    def GetNumber4(self):
-        return str(d)
+    def GetNumber(self):
+        i = random.randint(0, len(data)-1)
+        return str(data.pop(i))
     
+    def GetNeff(self):
+        return str(len(data))
+
+    def GetCardsLeft(self):
+        return 'Cards Left : ' + self.GetNeff()
+
     def CardsReset(self):
+        global data
+        global datainit
+
         self.ids.label1.text = 'No card'
         self.ids.label2.text = 'No card'
         self.ids.label3.text = 'No card'
         self.ids.label4.text = 'No card'
+        data = copy.copy(datainit)
+        self.ids.cardsleftlabel.text = self.GetCardsLeft()
 
-    pass
-
-class ButtonInputScreen(Screen):
     pass
 
 class AnswerScreen(Screen):
+
     def Solve(self):
-        return FileInputScreen().GetNumber1() + '+' + FileInputScreen().GetNumber2() + '+' + FileInputScreen().GetNumber3() + '+' + FileInputScreen().GetNumber4()
+        return "Hello, World!"
     
     def Points(self):
         return str(15)
     pass
 
 # Create the screen manager
-sm = ScreenManager(transition=FadeTransition())
+sm = ScreenManager(transition=SlideTransition())
 sm.add_widget(MenuScreen(name='menu'))
 sm.add_widget(FileInputScreen(name='fileinput'))
-sm.add_widget(ButtonInputScreen(name='buttoninput'))
 sm.add_widget(AnswerScreen(name='answer'))
 
-class TestApp(App):
+class UIApp(App):
 
     def build(self):
+        self.title = '24 Game Solver'
         return sm
 
 
 if __name__ == '__main__':
-    TestApp().run()
+    UIApp().run()
 
