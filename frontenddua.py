bc62134371d82c1493b13c29cdcabd898466bd46
Ferdy Santoso <36350020+ferdysan360@users.noreply.github.com>
2019-02-13 01:35:36 +0700

    Add files final

--- frontenddua.py
+++ frontenddua.py
@@ -5,28 +5,39 @@
 #Library untuk menggunakan sys.argv
 #Command akan berupa : py namafile.py fileinput.txt fileoutput.txt
 import sys
+import backend
+
+# Inisialisasi list jawaban
+listofanswer = ['0','0']
 
 #Assign variabel yang berisi sys.argv(1), yaitu nama file eksternal yang ingin dibaca
 fileinput = sys.argv[1]
 
 #Membuka File Eksternal Input
-inputtemp = open(fileinput, "r") #File eksternal disiapkan untuk dibaca ("r")
-inputlist = inputtemp.read().split(' ') #Memasukkan setiap data dari file eksternal ke dalam list/array, dengan pemisah berupa spasi/whitespace
+inputtemp = open(fileinput, "r")  # File eksternal disiapkan untuk dibaca ("r")
+# Memasukkan setiap data dari file eksternal ke dalam list/array, dengan pemisah berupa spasi/whitespace
+inputlist = inputtemp.read().split(' ')
 
 #Iterasi untuk mengubah tipe data dari string menjadi integer
 for i in range(4):
-    inputlist[i] = int(inputlist[i]) #Typecast setiap elemen list menjadi integer
+    # Typecast setiap elemen list menjadi integer
+    inputlist[i] = int(inputlist[i])
 
-print(inputlist) #Mencetak isi list ke layar, untuk test
+print(inputlist)  # Mencetak isi list ke layar, untuk test
+
+# Menghitung list of integers
+listofanswer = backend.solve24(inputlist)
 
 #Assign variabel yang berisi sys.argv(2), yaitu nama file eksternal yang ingin ditulis
 fileoutput = sys.argv[2]
 
 #Membuka File Eksternal Output
-outputtemp = open(fileoutput, "w") #File eksternal disiapkan untuk ditulis ("w")
-outputtemp.write("4") #Menulis keluaran ke file eksternal, ditest dengan string "4"
+# File eksternal disiapkan untuk ditulis ("w")
+outputtemp = open(fileoutput, "w")
+
+# Menulis keluaran ke file eksternal, ditest dengan string "4"
+outputtemp.write(listofanswer[0])
 
 #Menutup File Eksternal
-inputtemp.close() #Tutup file input
-outputtemp.close() #Tutup file output
-
+inputtemp.close()  # Tutup file input
+outputtemp.close()  # Tutup file output
